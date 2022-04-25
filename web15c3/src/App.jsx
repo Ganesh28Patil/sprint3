89ac7f1865f3012ca70db728f183dc0383aaba5a
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-25 17:15:21 +0530

    eval 3

---
+++ web15c3/src/App.jsx
@@ -0,0 +1,80 @@
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { EmployeeList } from "./components/EmployeeList";
+import { EmployeeDetails } from "./components/EmployeeDetails";
+import { Admin } from "./components/Admin";
+// import { ProtectedRoute } from "./components/PrivateRoute";
+import { Navbar } from "./components/Navbar";
+import { Logout } from "./components/Logout";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>{/* Routes here */}</Routes>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import logo from './logo.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello Vite + React!</p>
+//         <p>
+//           <button type="button" onClick={() => setCount((count) => count + 1)}>
+//             count is: {count}
+//           </button>
+//         </p>
+//         <p>
+//           Edit <code>App.jsx</code> and save to test HMR updates.
+//         </p>
+//         <p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//           {' | '}
+//           <a
+//             className="App-link"
+//             href="https://vitejs.dev/guide/features.html"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Vite Docs
+//           </a>
+//         </p>
+//       </header> */}
+//     </div>
+//   )
+// }
+
+// export default App
